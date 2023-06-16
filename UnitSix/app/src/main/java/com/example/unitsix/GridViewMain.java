25e91a89f774a78d11bf66d756810a07eb473805
depakmagr <magardeepak606@gmail.com>
2023-06-16 08:00:56 +0545

    Grid View Main

---
+++ UnitSix/app/src/main/java/com/example/unitsix/GridViewMain.java
@@ -0,0 +1,38 @@
+package com.example.unitsix;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GridViewMain extends AppCompatActivity {
+    GridView gridView;
+
+    @Override
+    protected void onCreate(Bundle b){
+        super.onCreate(b);
+
+        setContentView(R.layout.grid_view_main);
+
+        gridView = findViewById(R.id.grid_view);
+
+        String[] names = {"Person 1", "Person 2", "Person 3", "Person 4", "Person 5",
+                "Person 1", "Person 2", "Person 3", "Person 4", "Person 5",
+                "Person 1", "Person 2", "Person 3", "Person 4", "Person 5",
+                "Person 1", "Person 2", "Person 3", "Person 4", "Person 5",
+                "Person 1", "Person 2", "Person 3", "Person 4", "Person 5",
+                "Person 1", "Person 2", "Person 3", "Person 4", "Person 5",
+                "Person 1", "Person 2", "Person 3", "Person 4", "Person 5",
+                "Person 1", "Person 2", "Person 3", "Person 4", "Person 5",
+                "Person 1", "Person 2", "Person 3", "Person 4", "Person 5"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.grid_view_item, R.id.grid_view_text, names);
+
+        gridView.setAdapter(adapter);
+
+
+    }
+}
+
